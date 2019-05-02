f50cbb45ffe585eb30824d1eea074b1a07404363
Muhammad <mkamalkayani@gmail.com>
2019-05-02 19:34:20 +0200

    chore

---
+++ src/MockDict.js
@@ -0,0 +1,14 @@
+let iphoneColor = {
+  Stonegrey: "Dark Grey",
+  "Midnight Black": "Black",
+  "Mystic Silver": "Silver"
+};
+
+let iphoneColor2 = {
+  Stonegrey: "Dark Grey",
+  "Midnight Black": "Black",
+  "Mystic Silver": "Silver"
+};
+
+let MockDict = { iphoneColor, iphoneColor2 };
+export default MockDict;
