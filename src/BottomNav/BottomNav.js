cc15026fb93b59ebf1e8b80f12fa796f1d10b905
Muhammad <mkamalkayani@gmail.com>
2019-05-13 16:52:41 +0200

    feat: BottomNav compoenent

---
+++ src/BottomNav/BottomNav.js
@@ -0,0 +1,76 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import HomeIcon from "@material-ui/icons/Home";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+
+const styles = {
+  root: {
+    width: "100%",
+    margin: "auto",
+    marginTop: "20px",
+    position: "absolute",
+    left: "0",
+    bottom: "0",
+    backgroundColor: "#8e9cea"
+  }
+};
+
+class BottomNav extends React.Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+    switch (value) {
+      case "back":
+        this.props.history.goBack();
+        break;
+      case "forward":
+        this.props.history.goForward();
+        break;
+      case "home":
+        this.props.history.push("/");
+        break;
+      default:
+        return;
+    }
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        color='primary'
+        value={value}
+        onChange={this.handleChange}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction
+          label='Back'
+          value='back'
+          icon={<ArrowBackIcon />}
+        />
+        <BottomNavigationAction label='Home' value='home' icon={<HomeIcon />} />
+        <BottomNavigationAction
+          label='Forward'
+          value='forward'
+          icon={<ArrowForwardIcon />}
+        />
+      </BottomNavigation>
+    );
+  }
+}
+
+BottomNav.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(BottomNav);
